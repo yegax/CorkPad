2cab9455b2d893bd91e445f98f671dc021ace5ed
yegax <valdesgpatricio@gmail.com>
2021-11-21 18:46:21 -0500

    Update to add multiple export functionalities.

--- js/html2canvas.js
+++ js/html2canvas.js
@@ -1,21 +1,61 @@
 $(document).ready(function(){
+    
+    function saveAs(uri, filename) {
+        var link = document.createElement('a');
+        if (typeof link.download === 'string') {
+            link.href = uri;
+            link.download = filename;
 
-	//pdf	
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        } else {
+            window.open(uri);
+        }
+    }
+
+	//PDF Export
 	$("#pdfDownload").click(function(){
 	
         html2canvas(document.getElementById("printDiv"), {
+            useCORS: true,
             onrendered: function(canvas) {
 
-                var imgData = canvas.toDataURL('image/png');
-                console.log('Report Image URL: '+imgData);
-                var doc = new jsPDF('p', 'mm', [210, 280]); //210mm wide and 297mm high
-                
-                doc.addImage(imgData, 'PNG', 10, 10);
-                doc.save('sample.pdf');
+                var pdfData = canvas.toDataURL('image/png');
+                var doc = new jsPDF('p', 'mm', [470, 557]); //210mm wide and 297mm high
+                doc.addImage(pdfData, 'PNG', 10, 10);
+                doc.save('CorkPad.pdf');
             }
         });
 
 	});
+
+	//PNG Export	
+	$("#pngDownload").click(function(){
+	
+        html2canvas(document.getElementById("printDiv"), {
+            useCORS: true,
+            onrendered: function(canvas) {
+                saveAs(canvas.toDataURL(), 'CorkPad.png'); 
+            }
+        });
+
+	});
+
+    //JPG Export	
+	$("#jpgDownload").click(function(){
+	
+        html2canvas(document.getElementById("printDiv"), {
+            useCORS: true,
+            onrendered: function(canvas) {
+                var imgData = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
+                saveAs(imgData, 'CorkPad.jpg');
+            }
+        });
+
+	});
+	
+    
 	
 	
 })
